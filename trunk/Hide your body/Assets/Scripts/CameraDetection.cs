601f2e51076484ce851f81d0d2e1dfaa540b8cc9
aris.goico@gmail.com <aris.goico@gmail.com@120e55dc-e779-ebcb-4987-f0c61937aa57>
2013-11-05 22:54:30 +0000

    Primera implementación de detección por cámara y movimiento de jugador.

---
+++ trunk/Hide your body/Assets/Scripts/CameraDetection.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDetection : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.layer == 8)		//Player layer
+        	Debug.Log("Collider " + other.transform.name + " entered the camera.");
+    }
+    
+    void OnTriggerExit(Collider other) {
+    	if (other.gameObject.layer == 8)		//Player layer
+        	Debug.Log("Collider " + other.transform.name + " exited the camera.");
+    }
+}
---
+++ trunk/Hide your body/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	
+	public float maxRotationLeft		= 0.0f;
+	public float maxRotationRight		= 0.0f;
+	public float rotationSpeed			= 1.0f;
+	
+	private bool rotatingLeft			= false;
+	private Quaternion rotationLeft;
+	private Quaternion rotationRight;
+	
+	void Start() {
+		float angleTemp = 0.0f;
+		Vector3 axisTemp = Vector3.zero;
+		this.transform.rotation.ToAngleAxis(out angleTemp, out axisTemp);
+		rotationLeft =  Quaternion.AngleAxis(angleTemp + maxRotationLeft, axisTemp + Vector3.up);
+		rotationRight = Quaternion.AngleAxis(angleTemp + maxRotationRight, axisTemp + Vector3.up);
+		if (Quaternion.Angle(rotationLeft, rotationRight) < 0.5f)
+			Debug.LogError("The 2 angles cannot be so similar in CameraMovement script!");
+	}
+	
+	void Update () {
+		if (rotatingLeft) {		//Rotating left
+			this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rotationLeft, rotationSpeed * Time.deltaTime);
+		}
+		else {
+			this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rotationRight, rotationSpeed * Time.deltaTime);
+		}
+		
+		if (Quaternion.Angle(this.transform.rotation, rotationLeft) < 0.5f && rotatingLeft)
+			rotatingLeft = false;
+		if (Quaternion.Angle(this.transform.rotation, rotationRight) < 0.5f && !rotatingLeft)
+			rotatingLeft = true;
+	}
+}
