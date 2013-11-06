034d0ace2aa44aa0fbb1ddcf719a929d28cff8e1
aris.goico@gmail.com <aris.goico@gmail.com@120e55dc-e779-ebcb-4987-f0c61937aa57>
2013-11-06 22:18:23 +0000

    Implementada la funcionalidad de que las cámaras detecten con máscara de capas y que sea por Raycast.

--- trunk/Hide your body/Assets/Scripts/CameraDetection.cs
+++ trunk/Hide your body/Assets/Scripts/CameraDetection.cs
@@ -2,14 +2,38 @@
 using System.Collections;
 
 public class CameraDetection : MonoBehaviour {
-
+	
+	public LayerMask mask 	= -1;
+	public bool detecting	= true;
+	
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.layer == 8)		//Player layer
-        	Debug.Log("Collider " + other.transform.name + " entered the camera.");
+		if (!detecting)
+			return;
+		int otherLayer = 1 << other.gameObject.layer; 
+		if ((otherLayer & mask.value) > 0) {		//Player layer
+        	Vector3 otherCenter = other.bounds.center;
+        	Ray ray = new Ray(this.transform.position, (otherCenter - this.transform.position).normalized);
+        	RaycastHit hitInfo;
+        	if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity)) {
+        		if (hitInfo.collider == other)
+        			Debug.Log("Collider " + other.transform.name + " hit by the camera on enter.");
+        	}
+        	
+        }
     }
     
     void OnTriggerExit(Collider other) {
-    	if (other.gameObject.layer == 8)		//Player layer
-        	Debug.Log("Collider " + other.transform.name + " exited the camera.");
+    	if (!detecting)
+			return;
+    	int otherLayer = 1 << other.gameObject.layer; 
+		if ((otherLayer & mask.value) > 0) {		//Player layer
+        	Vector3 otherCenter = other.bounds.center;
+        	Ray ray = new Ray(this.transform.position, (otherCenter - this.transform.position).normalized);
+        	RaycastHit hitInfo;
+        	if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity)) {
+        		if (hitInfo.collider == other)
+        			Debug.Log("Collider " + other.transform.name + " hit by the camera on exit.");
+        	}        	
+        }
     }
 }
