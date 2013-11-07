171b84b6a84a3379e82e292436e2304be3aaefea
marcoscallejafernandez@gmail.com <marcoscallejafernandez@gmail.com>
2013-11-07 14:03:37 +0000

    Añadida escena para probar el pathfinding Actualizado el doc con cambios

---
+++ Hide your body/Assets/Scripts/SeekerAI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeekerAI : MonoBehaviour {
+	
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<NavMeshAgent>().SetDestination(target.position);
+	}
+}
