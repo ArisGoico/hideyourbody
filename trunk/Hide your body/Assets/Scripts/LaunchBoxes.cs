fb050a4d81e8f93b0f0fbdf27c0d6bc78b0b452b
marcoscallejafernandez@gmail.com <marcoscallejafernandez@gmail.com@120e55dc-e779-ebcb-4987-f0c61937aa57>
2013-11-07 14:03:37 +0000

    Añadida escena para probar el pathfinding Actualizado el doc con cambios

---
+++ trunk/Hide your body/Assets/Scripts/LaunchBoxes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchBoxes : MonoBehaviour {
+	
+	public GameObject box;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetButtonDown("Fire1"))
+		{
+			/*Transform pos = transform;
+			pos.Translate(Vector3.forward,Space.Self);*/
+			//Vector3 pos2 = new Vector3(transform.position.x,transform.position.y,transform.position.z);
+			GameObject newBox = Instantiate(box,transform.position,transform.rotation) as GameObject;
+			newBox.transform.Translate(Vector3.forward,Space.Self);
+			
+		}
+	}
+}
