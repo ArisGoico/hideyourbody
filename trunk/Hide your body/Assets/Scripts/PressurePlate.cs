24c0593b48c01660697c0b3061fd46792d773f34
aris.goico@gmail.com <aris.goico@gmail.com@120e55dc-e779-ebcb-4987-f0c61937aa57>
2013-11-09 15:26:56 +0000

    Pressure plates funcionando para cámaras, guardias y el jugador. Nueva escena (PressurePlateTest.unity) creada para poder probar su funcionamiento con una camara (se pueden lanzar cajas con el ratón).

---
+++ trunk/Hide your body/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour {
+
+	public LayerMask mask 		= -1;
+	public bool detecting		= true;
+	
+	public GameObject triggers;
+	
+	public enum Actions {Deactivate = 0, Activate = 1, Other = 2};
+	public Actions actionIn		= Actions.Deactivate;
+	public Actions actionOut 	= Actions.Activate;
+	
+	void Start() {
+		if (triggers != null) {
+			if (triggers.tag == "Camera") {
+				//Its a camera
+				if (triggers.GetComponentInChildren<PressurePlateCamera>() == null) {
+					Debug.LogWarning("The pressure plate triggered Camera \"" + triggers.name + "\" has no PressurePlateCamera script. Turning off pressure plate.");
+				}
+				else {
+					//Do something with the camera to initialize
+				}
+			}
+			else if (triggers.tag == "Player") {
+				//Its the player
+				if (triggers.GetComponentInChildren<PressurePlatePlayer>() == null) {
+					Debug.LogWarning("The pressure plate triggered Player \"" + triggers.name + "\" has no PressurePlatePlayer script. Turning off pressure plate.");
+				}
+				else {
+					//Do something with the player to initialize
+				}
+			}
+			else if (triggers.tag == "Guard") {
+				//Its a camera
+				if (triggers.GetComponentInChildren<PressurePlateGuard>() == null) {
+					Debug.LogWarning("The pressure plate triggered Guard \"" + triggers.name + "\" has no PressurePlateGuard script. Turning off pressure plate.");
+				}
+				else {
+					//Do something with the guard to initialize
+				}
+			}
+		}
+		else {
+			detecting = false;
+			Debug.LogWarning("The pressure plate " + this.gameObject.name + " has no trigger to activate. Turning off pressure plate.");
+		}
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (!detecting)
+			return;
+		int otherLayer = 1 << other.gameObject.layer; 
+		if ((otherLayer & mask.value) > 0) {		//Detection in the correct layer
+        	PressurePlateInterface script = null;
+        	if (triggers.tag == "Camera") {
+				//Its a camera
+				script = triggers.GetComponentInChildren<PressurePlateCamera>();
+				
+			}
+			else if (triggers.tag == "Player") {
+				//Its the player
+				script = triggers.GetComponentInChildren<PressurePlatePlayer>();
+			}
+			else if (triggers.tag == "Guard") {
+				//Its a camera
+				script = triggers.GetComponentInChildren<PressurePlateGuard>();
+			}
+			
+			if (script != null) {
+				switch (actionIn) {
+					case Actions.Deactivate: 
+						script.Deactivate();
+						break;
+					case Actions.Activate: 
+						script.Activate();
+						break;
+					case Actions.Other:
+						script.Other();
+						break;
+				}
+			}        	
+        }
+    }
+    
+    void OnTriggerExit(Collider other) {
+    	if (!detecting)
+			return;
+		int otherLayer = 1 << other.gameObject.layer; 
+		if ((otherLayer & mask.value) > 0) {		//Detection in the correct layer
+        	PressurePlateInterface script = null;
+        	if (triggers.tag == "Camera") {
+				//Its a camera
+				script = triggers.GetComponentInChildren<PressurePlateCamera>();
+				
+			}
+			else if (triggers.tag == "Player") {
+				//Its the player
+				script = triggers.GetComponentInChildren<PressurePlatePlayer>();
+			}
+			else if (triggers.tag == "Guard") {
+				//Its a camera
+				script = triggers.GetComponentInChildren<PressurePlateGuard>();
+			}
+			
+			if (script != null) {
+				switch (actionOut) {
+					case Actions.Deactivate: 
+						script.Deactivate();
+						break;
+					case Actions.Activate: 
+						script.Activate();
+						break;
+					case Actions.Other:
+						script.Other();
+						break;
+				}
+			}        	
+        }
+    }
+}
---
+++ trunk/Hide your body/Assets/Scripts/PressurePlateCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlateCamera : MonoBehaviour, PressurePlateInterface {
+	
+	private CameraDetection detection;
+	private CameraMovement movement;
+	
+	void Start() {
+		detection = this.GetComponentInChildren<CameraDetection>();
+		movement = this.GetComponentInChildren<CameraMovement>();
+	}
+	
+	public void Activate() {
+		detection.detecting = true;
+		movement.moving = true;
+	}
+	
+	public void Deactivate() {
+		detection.detecting = false;
+		movement.moving = false;
+	}
+	
+	public void Other() {
+	
+	}
+}
