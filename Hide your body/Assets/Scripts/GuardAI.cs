7a0f3cd637521105c10435a08ea5829887b3861f
marcoscallejafernandez@gmail.com <marcoscallejafernandez@gmail.com>
2013-11-10 12:36:45 +0000

    Añadida cámara a guardia. Añadida opción de cambiar waypoint actual del guardia en el editor.

--- Hide your body/Assets/Scripts/GuardAI.cs
+++ Hide your body/Assets/Scripts/GuardAI.cs
@@ -3,25 +3,30 @@ using System.Collections;
 
 public class GuardAI : MonoBehaviour {
 	
-	public Vector3[] wayPoints;
-	public Vector3 actualWayPoint;
+	public Vector3[] waypoints;
+	public int numCurrentWaypoint;
+	public Vector3 currentWaypoint;	
 	private NavMeshAgent agent;	
 		
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
-		if(wayPoints.Length > 0)
+		if(waypoints.Length > 0)
 		{			
-			actualWayPoint = wayPoints[Random.Range(0,wayPoints.Length)];
+			currentWaypoint = waypoints[Random.Range(0,waypoints.Length)];
 		}
 	}
 	
 	void FixedUpdate () {
-		if(transform.position.x == actualWayPoint.x && transform.position.z == actualWayPoint.z)
-		{
-			int pos = Random.Range(0,wayPoints.Length);
-			//Debug.Log("Pos nº:"+pos+" vector:"+wayPoints[pos]);
-			actualWayPoint = wayPoints[pos];
+		if(waypoints.Length > 0)
+		{			
+			if(transform.position.x == currentWaypoint.x && transform.position.z == currentWaypoint.z)
+			{
+				numCurrentWaypoint = Random.Range(0,waypoints.Length);
+				//Debug.Log("Pos nº:"+pos+" vector:"+waypoints[pos]);
+				currentWaypoint = waypoints[numCurrentWaypoint];
+			}
+			agent.SetDestination(waypoints[numCurrentWaypoint]);
+			//agent.SetDestination(currentWaypoint);
 		}
-		agent.SetDestination(actualWayPoint);
 	}
 }
