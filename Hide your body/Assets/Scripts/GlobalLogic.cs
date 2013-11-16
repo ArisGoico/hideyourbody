ae361acaf52f743050d4390c7ea316459b1bcecf
marcoscallejafernandez@gmail.com <marcoscallejafernandez@gmail.com>
2013-11-16 07:29:43 +0000

    Añadido script de control general Añadida salida

---
+++ Hide your body/Assets/Scripts/GlobalLogic.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalLogic : MonoBehaviour {
+	
+	public Vector3 startPoint;
+	private bool alarm;
+	
+	// Use this for initialization
+	void Start () {
+		alarm = false;
+		transform.position = startPoint;
+		GetComponent<FPSInputController>().enabled = true;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(alarm)
+		{
+			//LUZ AMBIENTAL EN ROJO
+			//SONIDO DE ALARMA
+			if(Input.GetMouseButtonDown(0))
+				resetLevel();
+		}
+	}
+	
+	public void levelCompleted()
+	{
+		Debug.Log("Level completed");
+		//CAMBIAR A SIGUIENTE NIVEL
+	}
+	
+	public void playerDetected()
+	{
+		Debug.Log("Player detected: Game Over. Press mouse left button to restart level.");
+		GetComponent<FPSInputController>().enabled = false;
+		alarm = true;		
+	}
+	
+	public void resetLevel()
+	{
+		alarm = false;
+		transform.position = startPoint;
+		GetComponent<FPSInputController>().enabled = true;			
+	}
+}
