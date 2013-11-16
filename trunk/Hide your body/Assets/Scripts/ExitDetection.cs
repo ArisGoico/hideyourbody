20af9466505bac70a05ad3b8d671d0cae9b93b23
marcoscallejafernandez@gmail.com <marcoscallejafernandez@gmail.com@120e55dc-e779-ebcb-4987-f0c61937aa57>
2013-11-16 07:29:43 +0000

    Añadido script de control general Añadida salida

---
+++ trunk/Hide your body/Assets/Scripts/ExitDetection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitDetection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	private void OnTriggerEnter(Collider other) 
+	{
+		int playerLayer = LayerMask.NameToLayer("Player"); 
+		if (playerLayer == other.gameObject.layer) 
+		{
+			other.gameObject.GetComponent<GlobalLogic>().levelCompleted();
+		}
+	}
+}
