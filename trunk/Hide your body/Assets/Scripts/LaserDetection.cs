691f4c88822351cbde2cffe1aa0167e4ae6d91f3
marcoscallejafernandez@gmail.com <marcoscallejafernandez@gmail.com@120e55dc-e779-ebcb-4987-f0c61937aa57>
2013-11-30 13:17:02 +0000

    Añadidos láseres. Modificada detección para bloquear correctamente el control del jugador.

---
+++ trunk/Hide your body/Assets/Scripts/LaserDetection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserDetection : MonoBehaviour {
+	
+	private LaserCreation laserCreation;
+	// Use this for initialization
+	void Start () {
+		laserCreation = GetComponent<LaserCreation>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		int playerLayer = LayerMask.NameToLayer("Player"); 		
+		Ray ray = new Ray(laserCreation.laserStartPoint,laserCreation.laserEndPoint);
+		RaycastHit hit;		
+		if(Physics.Raycast(ray, out hit, 1000)) 
+		{
+        	if(hit.collider.gameObject.layer == playerLayer)
+			{
+				Debug.Log("Player detected by laser");
+				GameObject.Find("Player").GetComponent<GlobalLogic>().playerDetected();
+			}
+        }
+	}
+}
